8ceaf44cb3c17a9a6c723389007d7d2a138155c9
TimRiebesam <tim.riebesam@aol.de>
2020-06-24 14:17:41 +0200

    Globals AWS API-Pfad hinzugefügt

--- assets/js/app/globals.js
+++ assets/js/app/globals.js
@@ -1,4 +1,5 @@
-var gastroMeApiUrl = "http://localhost:5000/";
+var gastroMeApiUrl = "http://localhost:5000/"; //local
+//var gastroMeApiUrl = "http://GastromeApi-env.eba-gdpwc2as.us-east-2.elasticbeanstalk.com/"; //aws
 var gastroMeApiAuthTokenValue = "4df6d7b9-ba79-4ae7-8a1c-cffbb657610a";
 
 var currentRestaurant;
