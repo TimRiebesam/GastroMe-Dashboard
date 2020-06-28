30e40ae774465afcb6c0c245e915c8e947e36ea5
TimRiebesam <tim.riebesam@aol.de>
2020-06-28 14:50:06 +0200

    ApiUrl in globals angepasst

--- assets/js/app/globals.js
+++ assets/js/app/globals.js
@@ -3,8 +3,8 @@ var restaurantViewTitle = "GastroMe - Restaurant";
 var foodViewTitle = "GastroMe - Foods";
 var drinkViewTitle = "GastroMe - Drinks";
 
-var gastroMeApiUrl = "http://localhost:5000/"; //local
-//var gastroMeApiUrl = "http://GastromeApi-env.eba-gdpwc2as.us-east-2.elasticbeanstalk.com/"; //aws
+//var gastroMeApiUrl = "http://localhost:5000/"; //local
+var gastroMeApiUrl = "http://GastromeApi-prod.eba-gdpwc2as.us-east-2.elasticbeanstalk.com/"; //aws
 var gastroMeApiAuthTokenValue = "4df6d7b9-ba79-4ae7-8a1c-cffbb657610a";
 
 var currentRestaurant;
